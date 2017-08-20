ac89b857cd4097c34b9587019c8b53ad68f41139
Chivon <chhaichivon@2ntkh.com>
2017-08-20 15:54:28 +0700

    book codeigniter

---
+++ application/views/index.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Book to CodeIgniter</title>
+</head>
+<body>
+
+<div class="container">
+	<dev><h2>Book Codeignter Web App</h2></dev>
+</div>
+
+</body>
+</html>
